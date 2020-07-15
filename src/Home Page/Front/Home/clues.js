4bd22389256b0f3afff481854590fb8ac356cb6d
cmrnprry <perry.ca@husky.neu.edu>
2020-07-15 16:41:32 -0400

    added nico's assets and sarted on implementing them

--- src/Home Page/Front/Home/clues.js
+++ src/Home Page/Front/Home/clues.js
@@ -7,6 +7,16 @@ import { instanceOf } from 'prop-types';
 //Image Imports
 import Pen from '../../../folder_elements/pen/pen_bak.png'
 import RrosePhoto from '../../../Puzzles/cacodylic_eye/rrose_images/r_rose_thumbnail.png'
+import Folder1 from '../../../folder_elements/folder_pages/folder_1.png'
+import Folder2 from '../../../folder_elements/folder_pages/folder_2.png'
+import Folder3 from '../../../folder_elements/folder_pages/folder_3.png'
+import Folder4 from '../../../folder_elements/folder_pages/folder_4.png'
+import Folder5 from '../../../folder_elements/folder_pages/folder_5.png'
+import Folder6 from '../../../folder_elements/folder_pages/folder_6.png'
+import Folder7 from '../../../folder_elements/folder_pages/folder_7.png'
+import Folder8 from '../../../folder_elements/folder_pages/folder_8.png'
+import Folder9 from '../../../folder_elements/folder_pages/folder_9.png'
+import Folder10 from '../../../folder_elements/folder_pages/folder_10.png'
 
 //Web Imports
 import Image from 'react-bootstrap/Image'
@@ -34,12 +44,59 @@ class Clues extends Component {
     componentDidMount() {
         document.body.style.removeProperty("background");
         window.addEventListener("resize", this.resizeWindow);
+        this.resizeWindow();
     }
 
     //So the program always has the correct width and height of window
     resizeWindow() {
-        console.log("here")
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
+        console.log("height: " + window.innerHeight)
+        console.log("width: " + window.innerWidth)
+        var folders = document.getElementsByClassName("folder-scale");
+        var newWidth = 970;
+
+        if (window.innerWidth >= 1400) { //larges screen size
+            if (window.innerHeight >= 1080) { //larges screen size
+                newWidth = 1440;
+            }
+            else if (window.innerHeight >= 800) { //medium screen size
+                newWidth = 1100;
+
+            }
+            else { //smallest screen size
+                newWidth = 1000;
+            }
+        }
+        else if (window.innerWidth >= 1024) { //medium screen size
+            if (window.innerHeight >= 800) { //medium screen size
+                newWidth = 1100;
+
+            }
+            else if (window.innerHeight >= 720) { //smallest screen size
+                newWidth = 1000;
+            }
+            else {
+                newWidth = 855;
+            }
+
+        }
+        else { //smallest screen size
+            newWidth = 745;
+        }
+
+        // if (window.innerHeight >= 1400) { //larges screen size
+        //     newWidth = 1230;
+        // }
+        // else if (window.innerHeight >= 1024) { //medium screen size
+        //     newWidth = 970;
+
+        // }
+        // else if (window.innerHeight >= 722) { //smallest screen size
+        //     newWidth = 1015;
+        // }
+
+        for (var i = 0; i < folders.length; i++) {
+            document.getElementsByClassName("folder-scale")[i].style.width = newWidth + "px";
+        }
     }
 
     render() {
@@ -53,8 +110,9 @@ class Clues extends Component {
                 </Helmet>
 
 
-                <div className="folder">
+                <div className="folder center">
                     <Image src={Pen} className="pen" />
+                    <Image id="Folder" src={Folder1} className="folder-scale" />
 
                     {/* Links to puzzles */}
                     <div className="written" style={{ paddingTop: '55px' }}>
@@ -88,8 +146,8 @@ class Clues extends Component {
                     </div>
 
                     {/* Artifacts */}
-                        <div className="written-2">Secrets</div>
-                        {cookies.get('RroseArtifact') && <Image className="artifact-rose" src={RrosePhoto} />}
+                    <div className="written-2">Secrets</div>
+                    {cookies.get('RroseArtifact') && <Image className="artifact-rose" src={RrosePhoto} />}
                 </div>
 
 
